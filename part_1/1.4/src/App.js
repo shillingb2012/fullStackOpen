4e9203c676ae977b8df3c5c043ace276562f1908
shillingb2012 <61397566+shillingb2012@users.noreply.github.com>
2022-04-28 20:07:57 -0500

    Exercise 1.4
    Added array to store parts
    Refactored App return portion to send objects instead of key/value of objects

---
+++ part_1/1.4/src/App.js
@@ -0,0 +1,63 @@
+// Takes care of rendering the name of the course
+const Header = (props) => {
+  console.log(props);
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+// Part renders the part and exercise
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.e}
+    </p>
+  )
+}
+
+// Renders the parts and their number of exercises
+function Content(props) {
+  return (
+    <div>
+      <Part part={props.parts[0].name} e={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} e={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} e={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+// Renders total number of exercises
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+    </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development';
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  return (
+    // Okay to hard code in 3 items in the array instead of using array iteration
+    <div>
+      <Header course={course} />
+      <Content parts={parts} /> 
+      <Total parts={parts} />
+    </div>
+  )
+}
+export default App;
